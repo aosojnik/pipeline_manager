5e9a48fc5daa5483349430b223b015b9e7d27477
Aljaž Osojnik <aljaz.osojnik@ijs.si>
2024-04-17 16:10:26 +0200

    Removed leftover SAAM-specific functions

--- pm/features/functions/__init__.py
+++ pm/features/functions/__init__.py
@@ -1,8 +1,2 @@
 from .general import *
-from .timeseries_functions import *
 from .models import run_ml_model
-
-from .sleep_functions import *
-from .activity_functions import *
-from .social_functions import *
-from .mobility_functions import *
